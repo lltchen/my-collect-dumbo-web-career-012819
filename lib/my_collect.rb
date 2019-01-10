59936ef6174b93e02879d56d531174d7b2bea05f
lltchen <lltchen1@gmail.com>
2019-01-10 19:13:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,19 @@
 def my_collect(collection)
     i = 0
+<<<<<<< HEAD
     new_collection = []
   while i < collection.length
 
     yield(collection[i])
     new_collection.push(collection[i])
     i += 1
+=======
+  while i < collection.length
+    new_collection = []
+    yield(collection[i])
+    new_collection << collection[i]
+    i = i + 1
+>>>>>>> 095b0fb7b49367d789fed89b23c32b30af5c8146
   end
   new_collection
 end
