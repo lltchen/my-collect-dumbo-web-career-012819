98dfd4fb288bacda8a88b1b02698fbb31f319e5a
lltchen <lltchen1@gmail.com>
2019-01-10 19:04:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+    i = 0
+    new_collection = []
+  while i < collection.length
 
-
+    yield(collection[i])
+    new_collection.push(collection[i])
+    i += 1
+  end
+  new_collection
+end
